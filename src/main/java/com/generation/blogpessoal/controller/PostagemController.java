63f4821166966d1e32a5ba3a57cf9f07fe53d317
Rebeka <rebeka.silva.emailprofissional@gmail.com>
2025-01-25 10:07:07 -0300

    Método Buscar por id, título e cadastrar

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -3,14 +3,20 @@ package com.generation.blogpessoal.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
+
+import jakarta.validation.Valid;
 
 @RestController // Define que a classe receberá requisições: URL, Verbo, Corpo da requisição 
 @RequestMapping("/postagens") // Define a URL padrão do recurso (/postagens).Ao digitar a url do servidor seguida da url do Recurso (http://localhost:8080/postagens), o Spring envia a requisição para a Classe responsável pelo Recurso associado à este endereço.
@@ -23,6 +29,23 @@ public class PostagemController {
 		@GetMapping // Indiga que o método getAll(), responderá a todas ass requisições do tipo HTTP GET enviadas ao endereço http://localhost:8080/postagens/
 		public ResponseEntity<List<Postagem>> getAll(){
 			return ResponseEntity.ok(postagemRepository.findAll());
-			
+		}
+		
+		@GetMapping("/{id}")
+		public ResponseEntity<Postagem> getById(@PathVariable Long id) { //SELECT * FROM tb_postagens where id = id
+			return postagemRepository.findById(id)
+					.map(resposta -> ResponseEntity.ok(resposta))
+					.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+		}
+		
+		@GetMapping("/titulo/{titulo}")
+		public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo){
+			return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
+		}
+		
+		@PostMapping
+		public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem){
+			return ResponseEntity.status(HttpStatus.CREATED)
+					.body(postagemRepository.save(postagem));
 		}
 }
--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,6 +1,9 @@
 package com.generation.blogpessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.generation.blogpessoal.model.Postagem;
 
@@ -8,5 +11,7 @@ public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 		
 	// Classe Postagem: É a entidade que será mapeada no Banco de Dados (Referente a que criou a tabela)
 	// Long: Representa a Chave Primária (PK) que é o atributo que reecebeu a anotação @Id
+	
+	public List <Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo); //finb By Titulo String titulo
 	
 }
